fcc7c364f2e45ba042489a84f33813ec02bb4358
TRANHONGPHONG97 <phong12b5nd@gmail.com>
2022-05-16 16:01:12 +0700

    tai file

--- src/ArrayJava/AddNumber.java
+++ src/ArrayJava/AddNumber.java
@@ -1,21 +1,22 @@
 package ArrayJava;
+
 import java.util.Arrays;
-public class Add_Number {
+
+public class AddNumber {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5,};
+        int[] arr = {1, 2, 3, 4, 5};
         int x = 7;
-        int index = 2;
+        int index = 1;
         int[] temp = new int[6];
-        for (int i = 0; i < temp.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if (i < index)
                 temp[i] = arr[i];
             else if (i == index)
-                temp[i] = x;
+              temp[i] = x;
             else
                 temp[i] = arr[i - 1];
         }
-        arr = temp;
+      arr=temp;
         System.out.println(Arrays.toString(arr));
     }
 }
-
