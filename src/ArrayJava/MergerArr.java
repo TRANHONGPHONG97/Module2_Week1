fe9c59b1be60fc3b6b2fc8d26b4896b62ff69d44
TRANHONGPHONG97 <phong12b5nd@gmail.com>
2022-05-17 09:47:41 +0700

    up file

--- src/ArrayJava/MergerArr.java
+++ src/ArrayJava/MergerArr.java
@@ -6,33 +6,24 @@ import java.util.Scanner;
 public class MergerArr {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int[] arr1 = new int[2];
         int[] arr2 = new int[3];
-
         System.out.print("mang 1: " + "\n");
-        int k = 0;
-        while (k < arr1.length) {
-            System.out.print("phan tu: " + (k + 1) + ": ");
-            arr1[k] = scanner.nextInt();
-            k++;
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print("phan tu: " + (i + 1) + ": ");
+            arr1[i] = scanner.nextInt();
         }
-        int j = 0;
         System.out.print("mang 2: " + "\n");
-        while (j < arr2.length) {
+        for (int j = 0; j < arr2.length; j++) {
             System.out.print("phan tu: " + (j + 1) + ": ");
             arr2[j] = scanner.nextInt();
-            j++;
         }
-
         int[] arr3 = new int[arr1.length + arr2.length];
-        int i = 0;
-        for (i = 0; i < arr1.length + arr2.length; i++) {
-            if (i < arr1.length) {
-                arr3[i] = arr1[i];
-            } else {
-                arr3[i] = arr2[i - arr1.length];
-            }
+        for (int i = 0; i < arr1.length; i++) {
+            arr3[i] = arr1[i];
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            arr3[arr1.length + i] = arr2[i];
         }
         System.out.println("Mang gop la: " + Arrays.toString(arr3));
     }
